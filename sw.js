fc21de83e650bac14c3338c572de319748f831f0
F2K229 <f2kservice@gmail.com>
2026-03-20 09:25:57 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+var CACHE="f2k-v1";
+var FILES=["/","/index.html"];
+self.addEventListener("install",function(e){ e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(FILES); })); });
+self.addEventListener("fetch",function(e){ e.respondWith(caches.match(e.request).then(function(r){ return r||fetch(e.request).catch(function(){ return caches.match("/index.html"); }); })); });
+self.addEventListener("activate",function(e){ e.waitUntil(caches.keys().then(function(names){ return Promise.all(names.filter(function(n){ return n!==CACHE; }).map(function(n){ return caches.delete(n); })); })); });
